647533a96fb1147c9a3fe148d2052eac84a9de3f
federico.pecora@gmail.com <federico.pecora@gmail.com@ea067d57-9c1c-a4f3-5948-99583deb17b8>
2014-09-28 14:55:19 +0000

    First commit. --F.

---
+++ trunk/src/main/java/lab1/TestRubiksCube.java
@@ -0,0 +1,31 @@
+package lab1;
+import java.util.Calendar;
+
+import solutions.lab1.MaximumManhattanDistance;
+import aima.core.search.framework.GraphSearch;
+import aima.core.search.framework.Problem;
+import aima.core.search.framework.Search;
+import aima.core.search.framework.TreeSearch;
+import aima.core.search.informed.AStarSearch;
+import aima.core.search.uninformed.BreadthFirstSearch;
+
+public class TestRubiksCube {
+
+	public static void main(String[] args) {
+
+		RubiksCube rubiksCube = new RubiksCube(3, 2);
+		Problem problem = new Problem(rubiksCube, RubiksCubeFunctionFactory.getSymmetricActionsFunction(), 
+				RubiksCubeFunctionFactory.getResultFunction(), new RCgoalTest());
+
+		Search search = new BreadthFirstSearch(new TreeSearch());
+		
+		long start = Calendar.getInstance().getTimeInMillis();
+		try {
+			System.out.println("SOLUTION: " + search.search(problem));
+			System.out.println("Metrics: " + search.getMetrics());
+			System.out.println("Time [msec]: " + (Calendar.getInstance().getTimeInMillis()-start));
+		}
+		catch (Exception e) { e.printStackTrace(); }
+	}
+
+}
---
+++ trunk/src/main/java/solutions/lab1/MisplacedCubiesAdmissible.java
@@ -0,0 +1,39 @@
+package solutions.lab1;
+import lab1.RubiksCube;
+import aima.core.search.framework.HeuristicFunction;
+
+public class MisplacedCubiesAdmissible implements HeuristicFunction {
+
+	private int [][][] goal;
+	private int n;
+	
+	public MisplacedCubiesAdmissible(RubiksCube rc) {
+		n = rc.getState().length;
+		goal = new int[n][n][n];
+		int counter = 1;
+		for (int i = 0; i < n; i++){
+			for (int j = 0; j < n; j++){
+				for (int k = 0; k < n; k++){
+					goal[i][j][k] = counter;
+					counter++;
+				}
+			}
+		}
+	}
+	
+	
+	@Override
+	public double h(Object state) {
+		RubiksCube rc = (RubiksCube) state;	
+		int retVal = 0;
+		for (int i = 0; i < n; i++){
+			for (int j = 0; j < n; j++){
+				for (int k = 0; k < n; k++){
+					if (goal[i][j][k] != rc.getState()[i][j][k].getId()) retVal++;
+				}
+			}
+		}
+		return (((double)retVal)/8);
+	}
+	
+}
