adfbdd2bf93352e19c35745f8b509651f70b3666
Federico Pecora <federico.pecora@oru.se>
2015-09-22 17:44:09 +0200

    removed assignments from solution branch

--- src/main/java/lab1/Face.java
+++
@@ -1,7 +0,0 @@
-package lab1;
-public class Face {
-	private String color = "";
-	public Face(String color) { this.color = color; }
-	public String getColor() { return color; }
-	public void setColor(String color) { this.color = color; }
-}
