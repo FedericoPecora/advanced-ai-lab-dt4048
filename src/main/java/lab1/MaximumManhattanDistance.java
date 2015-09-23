cfacf8623dc7218c26b28b8f7ec150b52e222e66
iran <iran.mansoori85@gamil.com>
2015-09-23 13:44:30 +0200

    function added to identify corner and egde cubie

---
+++ src/main/java/lab1/MaximumManhattanDistance.java
@@ -0,0 +1,47 @@
+package lab1;
+import aima.core.search.framework.HeuristicFunction;
+
+public class MaximumManhattanDistance implements HeuristicFunction{
+
+	private int [][][] goal;
+	private int n;
+	
+	public MaximumManhattanDistance(RubiksCube rc) {
+		n = rc.getState().length;
+		goal = new int[n][n][n];
+		int counter = 1;
+		for (int i = 0; i < n; i++){
+			for (int j = 0; j < n; j++){
+				for (int k = 0; k < n; k++){
+					goal[i][j][k] = counter;
+					counter++;
+				}
+			}
+		}
+	}
+	
+	@Override
+	public double h(Object state) {
+		return 0;
+	}
+	
+	//this is specific to 3*3 cube
+	private boolean isCorner(int i, int j, int k){
+		
+		if((i == 0 || i == 2) && (j == 0 || j == 2) && (k == 0 || k == 2))
+			return true;
+		
+		return false;
+	}
+
+	//this is specific to 3*3 cube
+	private boolean isEdge(int i, int j, int k) {		
+		String[] edges = new String[]{"010","100","120","210","001","021","201","221", "012","102","122","212"};
+		
+		String res = String.valueOf(i).concat(String.valueOf(j).concat(String.valueOf(k)));
+		for (int l = 0; l < edges.length; l++) {
+			if(edges[l].equals(res)) return true;
+		}
+		return false;
+	}
+}
